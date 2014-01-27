b63c37d4e8d00fc0163b8b7faf1ed6145ecfec92
Gullit Miranda <gullitmiranda@requestdev.com.br>
2014-01-27 17:50:37 +0000

    fix ssl omniauth to heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :mercadolibre, "6308356104202656", "LX7hGeepzjp2kjhc2m9SqF5BnJ6C2TIg"
+  provider :mercadolibre, "6308356104202656", "LX7hGeepzjp2kjhc2m9SqF5BnJ6C2TIg",
+    :client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}
 end
